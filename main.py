1a53b27f2a8901dd1a289b9b2d465608c3640100
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-14 15:46:56 -0600

    Completed practice

---
+++ main.py
@@ -0,0 +1,33 @@
+# Create a new Python file and paste the following code into it. At the end write a for in loop to produce the output below.
+
+# OUTPUT
+# Penny: A penny for your thoughts
+# Injury: Add insult to injury
+# Moon: Once in a blue moon
+# Grape: I heard it through the grapevine
+# Murder: Kill two birds with one stone
+# Limbs: It costs an arm and a leg
+# Grain: Take what someone says with a grain of salt
+# Fences: I'm on the fence about it
+# Sheep: Pulled the wool over his eyes
+# Lucifer: Speak of the devil
+
+# Make sure you join the strings
+
+idioms = {
+    "Penny": ["A", "penny", "for", "your", "thoughts"],
+    "Injury": ["Add", "insult", "to", "injury"],
+    "Moon": ["Once", "in", "a", "blue", "moon"],
+    "Grape": ["I", "heard", "it", "through", "the", "grapevine"],
+    "Murder": ["Kill", "two", "birds", "with", "one", "stone"],
+    "Limbs": ["It", "costs", "an", "arm", "and", "a", "leg"],
+    "Grain": ["Take", "what", "someone", "says", "with", "a", "grain", "of", "salt"],
+    "Fences": ["I'm", "on", "the", "fence", "about", "it"],
+    "Sheep": ["Pulled", "the", "wool", "over", "his", "eyes"],
+    "Lucifer": ["Speak", "of", "the", "devil"],
+}
+separator = ""
+sentence = " "
+for word in idioms:
+
+    print(f"{separator.join(word)}: {sentence.join(idioms[word])}")
